e8783e27d575b8aa3263e0aa82914726c0e347f6
Siar Wahidi <siar.wahidi@gmail.com>
2022-04-20 17:04:54 -0400

    added list state and handle submit func, which still needs the functionality added

--- setup/src/App.js
+++ setup/src/App.js
@@ -6,6 +6,12 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
 
   return <h2>color generator project</h2>;
 }
